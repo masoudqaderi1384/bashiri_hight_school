0e4d88bb1af2efa1336985ebeaacc7cc1ec5005e
Masoud Qaderi <masoud.qaderi1384@gmail.com>
2023-08-12 14:52:47 +0430

    Haji Bashiri hight school

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff345fb4);
+const Color kSecondaryColor = Color(0xff6789ca);
+const Color kTextBlackColor = Color(0xff313131);
+const Color kTextWitheColor = Color(0xffffffff);
+const Color kContainerColor = Color(0xff777777);
+const Color kOtherColor = Color(0xfff4f6f7);
+const Color kTextLightColor = Color(0xffa5a5a5);
+const Color kErrorBorderColor = Color(0xffe74c3c);
+
+const kDefaultPassing = 20.0;
+
+const String mobilePattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 
+import 'package:bashiri_hight_school/constants.dart';
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
 
 void main() => runApp(SplashScreenApp());
 
@@ -11,66 +10,11 @@ class SplashScreenApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Splash Screen App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: '',
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        primaryColor: kPrimaryColor,
       ),
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => MyApp()),
-      );
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[800],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('images/school.png', width: 175,),
-            SizedBox(height: 50,),
-            Text("موسسه تعلیمی خصوصی \n حاجی عبدالظاهر بشیری و فرزندان",textAlign: TextAlign.center, style: TextStyle(
-              fontSize: 20,
-              fontFamily: 'Afghan',
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),),
-          ],
-        ),
-      ),
-    );
-  }
-}
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
